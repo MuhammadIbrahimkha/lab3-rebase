9815e36dcbe07fb84efa8e423c68a8d49cfe8ded
Instructor <instructor@class.local>
2026-09-12 17:58:46 +0500

    Update health check status

--- health_check.py
+++ health_check.py
@@ -5,7 +5,10 @@ Simple Python Health-Check Program
 
 def main():
     print("=== Health Check ===")
-    print("Status :Production")
+    print("Status : Production")
+
+    print("Status: Healthy - Monitoring")
+
 
 
 if __name__ == "__main__":
